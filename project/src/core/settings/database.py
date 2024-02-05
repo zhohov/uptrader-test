91fc722ecd11bfcf3af9c8d277a47574986a06b5
maksimzhohov <zhohovmaksim@gmail.com>
2024-02-05 18:15:53 +0300

    Initial commit

---
+++ project/src/core/settings/database.py
@@ -0,0 +1,10 @@
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+from core.settings import BASE_DIR
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
