2fc27a237251abf6a5aaab297268dc97ca1a29f0
maksimzhohov <zhohovmaksim@gmail.com>
2024-02-07 00:19:22 +0300

    Updated draw_menu

--- project/src/apps/menu/templatetags/menu_tags.py
+++ project/src/apps/menu/templatetags/menu_tags.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass
 from typing import List
 
 from django import template
-from django.shortcuts import get_object_or_404
 
 from apps.menu.models import Menu, MenuItem
 
@@ -11,13 +10,12 @@ register = template.Library()
 
 @dataclass
 class DrawMenuDataClass:
-    menu: Menu
+    menu_name: str
     menu_items: List[MenuItem]
 
 
 @register.simple_tag(takes_context=False)
 def draw_menu(menu_name: str) -> DrawMenuDataClass:
-    menu = get_object_or_404(Menu, name=menu_name)
-    menu_items = MenuItem.objects.all().select_related('menu').filter(menu=menu, parent=None)
+    menu_items = MenuItem.objects.filter(menu__name=menu_name, parent=None).select_related('menu')
 
-    return DrawMenuDataClass(menu=menu, menu_items=menu_items)
+    return DrawMenuDataClass(menu_name=menu_name, menu_items=menu_items)
